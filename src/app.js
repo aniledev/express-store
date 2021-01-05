618972206b62e53657db1ce870526a2a04ad102e
aniledev <elinamcgill@gmail.com>
2021-01-05 03:42:06 -0500

    FEAT: create a memory store for the post data

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -16,6 +17,24 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+//STORE DATA IN MEMORY IN PLACE OF DATABASE
+const users = [
+  {
+    id: "3c8da4d5-1597-46e7-baa1-e402aed70d80",
+    username: "sallyStudent",
+    password: "c00d1ng1sc00l",
+    favoriteClub: "Cache Valley Stone Society",
+    newsLetter: "true",
+  },
+  {
+    id: "ce20079c-2326-4f17-8ac4-f617bfd28b7f",
+    username: "johnBlocton",
+    password: "veryg00dpassw0rd",
+    favoriteClub: "Salt City Curling Club",
+    newsLetter: "false",
+  },
+];
 
 // ROUTES/ENDPOINTS
 app.post("/", (req, res) => {
@@ -67,6 +86,17 @@ app.post("/user", (req, res) => {
   if (!CLUBS.includes(favoriteClub)) {
     return res.status(400).send("Not a valid club");
   }
+
+  const id = uuid();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter,
+  };
+
+  users.push(newUser);
 
   //at this point in the pipeline all validation passed
   res.send("All validation passed");
