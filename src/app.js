3d6079f68d1458cbd55501ea73afe6234af7ae52
aniledev <elinamcgill@gmail.com>
2021-01-05 03:25:08 -0500

    FEAT: write validation for req.body parameters

--- src/app.js
+++ src/app.js
@@ -29,9 +29,47 @@ app.get("/", (req, res) => {
 
 app.post("/user", (req, res) => {
   // get the data
-  const { username, password, favoriteClub, newsLetter } = req.body;
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
 
-  // validation code here
+  const CLUBS = [
+    "Cache Valley Stone Society",
+    "Ogden Curling Club",
+    "Park City Curling Club",
+    "Salt City Curling Club",
+    "Utah Olympic Oval Curling Club",
+  ];
+
+  // validation code here; first consider if the value is required or not and if it is not required what do we do if it is not provided
+  if (!username) {
+    return res.status(400).send("Username required");
+  }
+
+  if (!password) {
+    return res.status(400).send("Password required");
+  }
+
+  if (!favoriteClub) {
+    return res.status(400).send("Favorite club required");
+  }
+
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send("Username must be between 6 and 20 characters");
+  }
+
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send("Password must be between 8 and 36 characters");
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send("Password must contain at least one digit");
+  }
+
+  if (!CLUBS.includes(favoriteClub)) {
+    return res.status(400).send("Not a valid club");
+  }
+
+  //at this point in the pipeline all validation passed
+  res.send("All validation passed");
 });
 
 // CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING; STILL MIDDLEWARE
