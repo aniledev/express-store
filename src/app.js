ff9d9126e2c3f4c5513bb867e6bb22001a3a8451
aniledev <elinamcgill@gmail.com>
2021-01-05 02:01:52 -0500

    CHORE: initialize project using express-boilerplate

---
+++ src/app.js
@@ -0,0 +1,37 @@
+// IMPORT REQUIRED LIBRARIES AND SECURITY PACKAGES
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+
+const app = express();
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+
+//STANDARD MIDDLEWARE
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING;
+//STILL MIDDLEWARE
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+//PIPELINE ENDS
+module.exports = app;
